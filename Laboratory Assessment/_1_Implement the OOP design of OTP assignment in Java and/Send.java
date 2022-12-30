354fcf3f4fff092be0aed7eb22724239ced4f125
Devanand <96509658+txcmosquito@users.noreply.github.com>
2022-12-30 12:01:06 +0530

    Update Send.java

--- Laboratory Assessment/_1_Implement the OOP design of OTP assignment in Java and/Send.java
+++ Laboratory Assessment/_1_Implement the OOP design of OTP assignment in Java and/Send.java
@@ -2,8 +2,8 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 public class Send {
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "AC27eccfc293255f6927dbc9e929bce6d5";
-    public static final String AUTH_TOKEN = "4b82234513c3d6a0d88e682d12838941";
+    public static final String ACCOUNT_SID = "*";
+    public static final String AUTH_TOKEN = "*";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
