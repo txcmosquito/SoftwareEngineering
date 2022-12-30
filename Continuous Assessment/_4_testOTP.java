3b8b5c66706608e4c0d1fc25c74401c668fa481c
Devanand <96509658+txcmosquito@users.noreply.github.com>
2022-12-30 14:36:17 +0530

    Update _4_testOTP.java

--- Continuous Assessment/_4_testOTP.java
+++ Continuous Assessment/_4_testOTP.java
@@ -2,23 +2,21 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class _3_testOTP {
-    // Declare all the variables
     String systemOTP, userOTP, s = "";
     int ranNo;
 
-    // Use getOTP( ) method to genarate random OTP
     public void getOTP(int len) {
-        // Use for loop to iterate 4 times and generate random OTP
+        
         for (int i = 0; i < len; i++) {
-            // Generate random digit within 0-9
+           
             ranNo = new Random().nextInt(9);
             s = s.concat(Integer.toString(ranNo));
         }
-        // Accept the generated OTP with systemOTP
+        
         systemOTP = s;
         System.out.println("System generated OTP is " + systemOTP);
         Scanner sc = new Scanner(System.in);
-        // Accept the user given OTP
+        
         System.out.print("Enter your OTP: ");
         userOTP = sc.nextLine();
         if (systemOTP.equals(userOTP))
@@ -29,7 +27,7 @@ public class _3_testOTP {
 
     public static void main(String[] args) {
         _3_testOTP otp = new _3_testOTP();
-        // Call getOTP( ) method
+        
         otp.getOTP(4);
     }
 
